a67d4b8b49a0977c6e1c00736b198e3230ba7c33
josevelaz <jose.carlos.velazquez@hotmail.com>
2023-05-11 18:53:51 -0400

    helpful message

--- misc/setup-tpm.sh
+++ misc/setup-tpm.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+echo "Make sure to run <prefix> + I inside a tmux session to install the plugins"
