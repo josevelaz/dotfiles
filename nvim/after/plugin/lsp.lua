b872026ef6fdf5f78ca577dda34492d615c90eca
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2023-05-12 13:15:57 -0400

    Changes made on Fri May 12 13:15:57 EDT 2023

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,6 +7,8 @@ end)
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
+require'lspconfig'.phpactor.setup{}
+
 lsp.ensure_installed({
     'tsserver',
     'eslint',
