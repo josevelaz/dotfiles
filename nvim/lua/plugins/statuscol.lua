ba4889a82d53c6c3c787358a54b80d519a1ec485
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-04-13 02:14:25 -0400

    added statuscol

---
+++ nvim/lua/plugins/statuscol.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				relculright = true,
+				segments = {
+					{ text = { " " } },
+					{
+						sign = {
+							name = { "Diagnostic" },
+							maxwidth = 1,
+							colwidth = 1,
+						},
+						click = "v:lua.ScSa",
+					},
+					{ text = { " " } },
+					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+					{ sign = { namespace = { "gitsign" } }, click = "v:lua.ScSa" },
+				},
+			})
+		end,
+	},
+}
