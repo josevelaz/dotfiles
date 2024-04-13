de401d2dda8c1ace53373c2f93849be3d8a9b2e8
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-04-13 00:10:17 -0400

    finished ts-tools

---
+++ nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,23 @@
+return {
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/inlayHint") then
+					vim.lsp.inlay_hint.enable(bufnr, true)
+				end
+			end,
+			settings = {
+				tsserver_file_preferences = {
+					includeInlayParameterNameHints = "all",
+					includeInlayFunctionLikeReturnTypeHints = true,
+				},
+				expose_as_code_action = "all",
+				jsx_close_tag = {
+					enable = true,
+				},
+			},
+		},
+	},
+}
