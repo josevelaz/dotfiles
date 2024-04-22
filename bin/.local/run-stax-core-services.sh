5722c637e8ebad8d6c0d85d6cd9ce9a90e9f28bb
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-04-22 11:01:00 -0400

    merge main into rework (#3)
    * random changes
    
    * kitty, karabiner and zsh changes

--- bin/.local/run-stax-core-services.sh
+++
@@ -1,6 +0,0 @@
-tmux \
-    new-session  'cd ~/repos/api/src && git switch master && composer install && php artisan serve' \; \
-    split-window -h 'cd ~/repos/permissions && git switch master && npm install && npm run start' \; \
-    split-window 'cd ~/repos/authservice && git switch master && npm install && npm run start' \; \
-    split-window -h 'cd ~/repos/transactions && git switch master && npm install && npm run start' \; \
-    split-window 'cd ~/repos/fattquery && git switch master && npm install && npm run start' \; \
