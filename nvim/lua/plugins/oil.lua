4b2387a02f5426d937e6ab3067d92708fbfbba2c
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-04-23 23:58:35 -0400

    sheesh, so many changes i cant remember, but its some good shit

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,9 +1,23 @@
 return {
 	{
 		"stevearc/oil.nvim",
+		init = function()
+			vim.keymap.set("n", "<leader>M", function()
+				local Oil = require("oil")
+				local filename = Oil.get_cursor_entry().name
+				local directory = Oil.get_current_dir()
+
+				local Grapple = require("grapple")
+				local Path = require("grapple.path")
+				Grapple.toggle({ path = Path.join(directory, filename) })
+			end, { desc = "Grapple tag under cursor" })
+		end,
 		opts = {
 			keymaps = {
 				["<Esc>"] = "actions.parent",
+				["<C-r>"] = "actions.refresh",
+				["<C-v>"] = "actions.select_vsplit",
+				["<C-s>"] = "actions.select_split",
 			},
 			view_options = {
 				show_hidden = true,
