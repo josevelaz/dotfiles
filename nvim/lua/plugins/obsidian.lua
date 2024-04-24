f89cf35fd3fc4dba00e024e247fc46f4cf52ee69
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-04-23 23:58:35 -0400

    sheesh, so many changes i cant remember, but its some good shit

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,20 @@
+return {
+	"epwalsh/obsidian.nvim",
+	version = "*", -- recommended, use latest release instead of latest commit
+	lazy = true,
+	cmd = { "ObsidianOpen", "ObsidianNew" },
+	dependencies = {
+		-- Required.
+		"nvim-lua/plenary.nvim",
+
+		-- see below for full list of optional dependencies 👇
+	},
+	opts = {
+		workspaces = {
+			{
+				name = "work",
+				path = "~/work/itemize/itemize-notes",
+			},
+		},
+	},
+}
