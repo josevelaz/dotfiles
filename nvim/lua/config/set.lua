7a73fdacca3c64ff5971911901d460c4260b2617
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-05-15 00:22:02 -0400

    wezterm migratio

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -19,6 +19,9 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "number"
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 12
