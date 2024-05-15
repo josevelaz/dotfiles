60dae2dba5738e961d3806d8ce09bbf9470a3e52
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-05-15 00:22:02 -0400

    wezterm migratio

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,180 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+local a = wezterm.action
+
+local function is_inside_vim(pane)
+	local tty = pane:get_tty_name()
+	if tty == nil then
+		return false
+	end
+
+	local success, stdout, stderr = wezterm.run_child_process({
+		"sh",
+		"-c",
+		"ps -o state= -o comm= -t"
+			.. wezterm.shell_quote_arg(tty)
+			.. " | "
+			.. "grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?)(diff)?$'",
+	})
+
+	return success
+end
+
+local function is_outside_vim(pane)
+	return not is_inside_vim(pane)
+end
+
+local function bind_if(cond, key, mods, action)
+	local function callback(win, pane)
+		if cond(pane) then
+			win:perform_action(action, pane)
+		else
+			win:perform_action(a.SendKey({ key = key, mods = mods }), pane)
+		end
+	end
+
+	return { key = key, mods = mods, action = wezterm.action_callback(callback) }
+end
+
+config.color_scheme = "Catppuccin Macchiato"
+
+config.window_decorations = "RESIZE"
+
+config.tab_bar_at_bottom = true
+
+config.font = wezterm.font("IosevkaTerm Nerd Font Mono", { weight = "Medium" })
+
+config.underline_position = "200%"
+config.underline_thickness = "250%"
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	bottom = "-1cell",
+}
+
+config.font_size = 14
+
+config.leader = { key = "a", mods = "CTRL" }
+
+config.keys = {
+	{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
+	{ key = "-", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "\\", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "v", mods = "LEADER", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "m", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+	-- { key = "j", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+	--  { key = "h", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+	-- { key = "k", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+	-- { key = "l", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+	{ key = "H", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
+	{ key = "J", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
+	{ key = "K", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
+	{ key = "L", mods = "LEADER|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
+	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+	bind_if(is_outside_vim, "h", "CTRL", a.ActivatePaneDirection("Left")),
+	bind_if(is_outside_vim, "j", "CTRL", a.ActivatePaneDirection("Up")),
+	bind_if(is_outside_vim, "k", "CTRL", a.ActivatePaneDirection("Down")),
+	bind_if(is_outside_vim, "l", "CTRL", a.ActivatePaneDirection("Right")),
+
+	{
+		key = "f",
+		mods = "CTRL",
+		action = wezterm.action_callback(function(window, pane)
+			local success, stdout, stderr = wezterm.run_child_process({
+				"/Users/josevelazquez/dotfiles/bin/.local/wezterm-workspaces.sh",
+			})
+			local workspaces = {}
+
+			if success then
+				for line in stdout:gmatch("[^\r\n]+") do
+					-- match on the '$' symbol
+					for line_id, line_label in line:gmatch("(.+) %$ (.+)") do
+						table.insert(workspaces, { id = line_id, label = line_label })
+					end
+				end
+			else
+				wezterm.log_info("stderr:", stderr)
+			end
+
+			window:perform_action(
+				a.InputSelector({
+					action = wezterm.action_callback(function(inner_window, inner_pane, id, label)
+						if not id and not label then
+							-- wezterm.log_info 'Cancelled'
+						else
+							-- wezterm.log_info('You selected id(' .. id .. ') and label(' .. label ..')')
+							inner_window:perform_action(
+								a.SwitchToWorkspace({
+									name = label,
+									spawn = {
+										label = "Workspace: " .. label,
+										cwd = id,
+									},
+								}),
+								inner_pane
+							)
+						end
+					end),
+					title = "Choose Workspace",
+          fuzzy_description = "  Search for Workspace: ",
+					choices = workspaces,
+          fuzzy = true
+				}),
+				pane
+			)
+		end),
+	},
+}
+
+wezterm.plugin.require("https://github.com/nekowinston/wezterm-bar").apply_to_config(config, {
+	position = "bottom",
+	max_width = 32,
+	dividers = "rounded", -- or "slant_left", "arrows", "rounded", false
+	indicator = {
+		leader = {
+			enabled = true,
+			off = " ",
+			on = " ",
+		},
+		mode = {
+			enabled = true,
+			names = {
+				resize_mode = "RESIZE",
+				copy_mode = "VISUAL",
+				search_mode = "SEARCH",
+			},
+		},
+	},
+	tabs = {
+		numerals = "arabic", -- or "roman"
+		pane_count = "superscript", -- or "subscript", false
+		brackets = {
+			active = { "", ":" },
+			inactive = { "", ":" },
+		},
+	},
+	clock = { -- note that this overrides the whole set_right_status
+		enabled = true,
+		format = "%H:%M", -- use https://wezfurlong.org/wezterm/config/lua/wezterm.time/Time/format.html
+	},
+})
+
+return config
