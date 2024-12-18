f30fdfc65b9a5c79f91921e1c1c293b8ae3af243
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2024-12-18 09:00:00 -0500

    chore: backup

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -1,3 +1,14 @@
+-- First define the sign
+vim.fn.sign_define("DapBreakpoint", {
+	text = "●", -- or "•" if you prefer a smaller dot
+	texthl = "DapBreakpointSign",
+	linehl = "",
+	numhl = "",
+})
+
+-- Create the highlight group with red color
+vim.api.nvim_set_hl(0, "DapBreakpointSign", { fg = "#FF0000" })
+
 return {
 	{
 		"luukvbaal/statuscol.nvim",
@@ -16,6 +27,14 @@ return {
 						click = "v:lua.ScSa",
 					},
 					{ text = { " " } },
+					{
+						sign = {
+							name = { "Dap" }, -- Will match DapBreakpoint, DapStopped, etc
+							maxwidth = 1,
+							colwidth = 1,
+						},
+						click = "v:lua.ScSa",
+					},
 					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
 					{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
 					{ sign = { namespace = { "gitsign" } }, click = "v:lua.ScSa" },
