4b2a2b3c34eeebeaaa2ddb5572e7283b5b28f162
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2025-01-24 09:02:29 -0500

    chore: backup

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -85,6 +85,7 @@ return {
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>sy", builtin.lsp_document_symbols, { desc = "[S]earch LSP S[y]mbols" })
 			vim.keymap.set("n", "<leader>so", "<cmd>Telescope oil<CR>", { desc = "[S]earch [O]il" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
