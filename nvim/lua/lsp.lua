3009233af1fdfc24b59c7f554f065700c337c085
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2025-02-04 09:00:01 -0500

    chore: backup

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -6,27 +6,39 @@ local on_attach = function(client, bufnr)
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
 	end
 
-	map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-	-- Find references for the word under your cursor.
-	map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-
-	-- Jump to the implementation of the word under your cursor.
-	--  Useful when your language has ways of declaring types without an actual implementation.
-	map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-
-	-- Jump to the type of the word under your cursor.
-	--  Useful when you're not sure what type a variable is and you want to see
-	--  the definition of its *type*, not where it was *defined*.
-	map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-	-- Fuzzy find all the symbols in your current document.
-	--  Symbols are things like variables, functions, types, etc.
-	map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-	-- Fuzzy find all the symbols in your current workspace.
-	--  Similar to document symbols, except searches over your entire project.
-	map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+	map("gd", function()
+		Snacks.picker.lsp_definitions()
+	end, "[G]oto [D]efinition")
+	--
+	-- -- Find references for the word under your cursor.
+	map("gr", function()
+		Snacks.picker.lsp_references()
+	end, "[G]oto [R]eferences")
+	--
+	-- -- Jump to the implementation of the word under your cursor.
+	-- --  Useful when your language has ways of declaring types without an actual implementation.
+	map("gI", function()
+		Snacks.picker.lsp_implementations()
+	end, "[G]oto [I]mplementation")
+	--
+	-- -- Jump to the type of the word under your cursor.
+	-- --  Useful when you're not sure what type a variable is and you want to see
+	-- --  the definition of its *type*, not where it was *defined*.
+	map("<leader>D", function()
+		Snacks.picker.lsp_type_definitions()
+	end, "Type [D]efinition")
+	--
+	-- -- Fuzzy find all the symbols in your current document.
+	-- --  Symbols are things like variables, functions, types, etc.
+	map("<leader>ds", function()
+		Snacks.picker.lsp_symbols()
+	end, "[D]ocument [S]ymbols")
+	--
+	-- -- Fuzzy find all the symbols in your current workspace.
+	-- --  Similar to document symbols, except searches over your entire project.
+	map("<leader>ws", function()
+		Snacks.picker.lsp_symbols({ workspace = true, finder = "lsp_symbols", format = "lsp_symbols" })
+	end, "[W]orkspace [S]ymbols")
 
 	-- Rename the variable under your cursor.
 	--  Most Language Servers support renaming across files, etc.
---
+++ nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,15 @@
+--- @type LazyPluginSpec
+return {
+	"folke/lazydev.nvim",
+	ft = "lua", -- only load on lua files
+	opts = {
+		library = {
+			"lazy.nvim",
+			{ path = "snacks.nvim", words = { "Snacks" } },
+			-- See the configuration section for more details
+			-- Load luvit types when the `vim.uv` word is found
+			{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			{ path = "wezterm-types", mods = { "wezterm" } },
+		},
+	},
+}
