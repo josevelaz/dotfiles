f702aecb4caeb1b311125c4b539755589feb1238
Jose Velazquez <jose.carlos.velazquez@hotmail.com>
2025-05-14 09:00:00 -0400

    chore: backup

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -11,3 +11,9 @@ vim.api.nvim_create_user_command("WorktreeAdd", function(args)
 end, {
 	nargs = "?",
 })
+
+vim.api.nvim_create_user_command("RelativePath", function()
+	local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":.")
+	vim.fn.setreg("+", path)
+	print("Copied: " .. path)
+end, {})
